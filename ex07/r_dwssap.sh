80d47e1fb6f13f67bf752069719a5e520f5773a5
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-07 15:10:02 +0200

    corrections

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
-cat /etc/passwd | tail -n +11 | awk "(NR+1)%2" | awk -F : '{print $1}' | rev | sort -r | head -n $FT_LINE2 | tail -n +$FT_LINE1 |  tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g'
+cat /etc/passwd | tail -n +11 | awk "(NR+1)%2" | awk -F : '{print $1}' | rev | sort -r | head -n $FT_LINE2 | tail -n +$FT_LINE1 |  tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g' | tr -d '\n'      
 # cat /etc/passwd       -> original output
 # tail -n +11           -> remove the boxed comment 11 lines long
 # awk "(NR+1)%2"        -> keep every other line starting from the 2nd
@@ -10,3 +10,4 @@ cat /etc/passwd | tail -n +11 | awk "(NR+1)%2" | awk -F : '{print $1}' | rev | s
 # tr '\n' ','           -> replace newlines with a comma
 # sed 's/,/, /g'        -> replace ',' with ', '
 # sed 's/, $/./g'       -> replace the last ', ' with ','
+# tr -d '\n'            -> delete the ending newline
