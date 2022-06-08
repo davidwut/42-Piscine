b3dcab2ec352f23f1ace62d936e2189096876c23
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-08 13:34:27 +0200

    ex04 done

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwuthric <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 13:09:49 by dwuthric          #+#    #+#             */
+/*   Updated: 2022/06/08 13:09:53 by dwuthric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	c;
+
+	if (n < 0)
+	{
+		c = 'N';
+		write(1, &c, 1);
+	}
+	else
+	{
+		c = 'P';
+		write(1, &c, 1);
+	}
+}
+
+#ifdef og
+
+int	main(void)
+{
+	ft_is_negative(-5);
+	ft_is_negative(0);
+	ft_is_negative(42);
+	return (0);
+}
+
+#endif
