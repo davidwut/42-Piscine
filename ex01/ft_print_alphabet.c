3291461a12fa39c28154cda0426e2a8c3cfe4587
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-08 12:53:56 +0200

    ex01 done

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwuthric <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 12:08:58 by dwuthric          #+#    #+#             */
+/*   Updated: 2022/06/08 12:09:00 by dwuthric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	int	i;
+
+	i = 97;
+	while (i < 97 + 26)
+	{
+		write(1, &i, 1);
+		i++;
+	}
+}
+
+#ifdef og
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
+
+#endif
