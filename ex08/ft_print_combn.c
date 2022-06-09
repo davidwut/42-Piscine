eda68386808b20c119862336a1b3b99111384e06
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-09 15:44:03 +0200

    works with 1,2,3, needs some fixes

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -20,15 +20,22 @@ void	putchar(char c)
 void	putformat(int *x, int n)
 {
 	int	i;
+	int	is_last_num;
 
+	is_last_num = 1;
 	i = 0;
 	while (i < n)
 	{
 		putchar(x[i] + '0');
+		if (x[i] != 10 - n + i)
+			is_last_num = 0;
 		i++;
 	}
-	putchar(',');
-	putchar(' ');
+	if (!is_last_num)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 }
 
 void	match_and_print(int *arr, int n)
@@ -58,25 +65,26 @@ void	ft_print_combn(int n)
 	int	x[10];
 	int	i;
 
-	x[0] = 0;
 	i = 0;
+	while (++i <= n)
+		x[i - 1] = i - 1;
 	while (x[0] <= 10 - n)
 	{
-		while (i < n)
-		{
-			if (i)
-				x[i] = x[i - 1] + 1;
-			i++;
-		}
-		i--;
-		while (x[i] <= 10 - n + i)
+		while (x[n - 1] <= 9)
 		{
 			match_and_print(x, n);
-			x[i]++;
+			x[n - 1]++;
 		}
-		if (i)
-			x[i - 1]++;
+		i = n - 1;
+		while (i > 0)
+		{
+			if (x[i] >= 10 - n + i)
+			{
+				x[i - 1]++;
+				x[i] = x[i - 1] + 1;
+			}
 			i--;
+		}
 	}
 }
 
@@ -84,7 +92,7 @@ void	ft_print_combn(int n)
 
 int	main(void)
 {
-	ft_print_combn(3);
+	ft_print_combn(5);
 	return (0);
 }
 
