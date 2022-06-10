d67c09efa3ce1acf39ea817a874c7bf3009bd0fa
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-10 14:19:21 +0200

    compilette conforming

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 13:50:54 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/10 14:13:48 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/10 14:18:27 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < size - 1)
