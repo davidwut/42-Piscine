2b0a8efcdf032faee63a6154bdee8c3856259eac
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-10 13:50:06 +0200

    corrected

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,32 +6,32 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:57:12 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/10 13:04:36 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/10 13:48:56 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcapitalize(char *str)
 {
-	int	found_space;
+	int	need_cap;
 	int	correction;
 	int	i;
 
-	found_space = 1;
-	correction = 'a' - 'A';
+	need_cap = 1;
+	correction = 'A' - 'a';
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (found_space)
+		if (str[i] == ' ' || str[i] == '-' || str[i] == '+')
+			need_cap = 1;
+		else if ('0' <= str[i] && str[i] <= '9')
+			need_cap = 0;
+		if (need_cap)
 		{
 			if ('a' <= str[i] && str[i] <= 'z')
 			{
 				str[i] += correction;
-				found_space = 0;
+				need_cap = 0;
 			}
-		}
-		else if (str[i] == ' ')
-		{
-			found_space = 1;
 		}
 		i++;
 	}
