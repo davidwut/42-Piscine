bf6540d9b8178ed439cf5dfa1955d4f38898add1
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-10 13:41:26 +0200

    corrected

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -6,36 +6,20 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:37:59 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/10 13:08:54 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/10 13:38:13 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ex07_ft_str_is_lowercase(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!('a' <= str[i] && str[i] <= 'z'))
-		{
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
 
 char	*ft_strupcase(char *str)
 {
 	int	offset;
 	int	i;
 
-	offset = 'a' - 'A';
+	offset = 'A' - 'a';
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (ex07_ft_str_is_lowercase(&str[i]))
+		if ('a' <= str[i] && str[i] <= 'z')
 			str[i] += offset;
 		i++;
 	}
--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -6,36 +6,20 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:46:02 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/10 13:10:43 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/10 13:38:45 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ex08_ft_str_is_uppercase(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!('A' <= str[i] && str[i] <= 'Z'))
-		{
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
 
 char	*ft_strlowcase(char *str)
 {
 	int	offset;
 	int	i;
 
-	offset = 'A' - 'a';
+	offset = 'a' - 'A';
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (ex08_ft_str_is_uppercase(&str[i]))
+		if ('A' <= str[i] && str[i] <= 'Z')
 			str[i] += offset;
 		i++;
 	}
