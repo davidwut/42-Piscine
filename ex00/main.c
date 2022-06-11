722c413b51f744c004ed8c8185c8c9f35236c1a4
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-11 11:08:02 +0200

    initial logic

---
+++ ex00/main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 10:33:03 by dwuthric          #+#    #+#             */
+/*   Updated: 2022/06/11 11:07:24 by dwuthric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_line_formatted(int length, char a, char b, char c)
+{
+	int	i;
+
+	ft_putchar(a);
+	i = 0;
+	while (i < length - 1)
+	{
+		ft_putchar(b);
+		i++;
+	}
+	if (length > 1)
+		ft_putchar(c);
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	if (!x || !y)
+		return ;
+	print_line_formatted(x, 'A', 'B', 'C');
+	while (y > 1)
+	{
+		print_line_formatted(x, 'B', ' ', 'B');
+		y--;
+	}
+	print_line_formatted(x, 'C', 'B', 'A');
+}
+
+int	main(void)
+{
+	rush(5, 5);
+	return (1);
+}
