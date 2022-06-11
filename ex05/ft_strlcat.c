43a7d8f92130ece4bd0892431c864de5368d66f3
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-11 19:14:19 +0200

    done

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 18:51:18 by dwuthric          #+#    #+#             */
+/*   Updated: 2022/06/11 19:10:50 by dwuthric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dst, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	dst_len;
+
+	i = 0;
+	dst_len = ft_strlen(dst);
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + i);
+}
