a9b5e118c6524aa9fb1ccc17440f873d3f4de00a
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-13 17:21:35 +0200

    reread prompt and fixed

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,32 +6,62 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:57:12 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/10 13:48:56 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/13 17:20:51 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft09_str_is_alpha(char c)
+{
+	if (!('a' <= c && c <= 'z'))
+		if (!('A' <= c && c <= 'Z'))
+			return (0);
+	return (1);
+}
+
+int	ft09_str_is_lowercase(char c)
+{
+	if (!('a' <= c && c <= 'z'))
+		return (0);
+	return (1);
+}
+
+int	ft09_str_is_printable(char c)
+{
+	if (!(' ' <= c && c <= '~'))
+		return (0);
+	return (1);
+}
+
+int	ft09_str_is_numeric(char c)
+{
+	if (!('0' <= c && c <= '9'))
+		return (0);
+	return (1);
+}
 
 char	*ft_strcapitalize(char *str)
 {
 	int	need_cap;
-	int	correction;
 	int	i;
 
 	need_cap = 1;
-	correction = 'A' - 'a';
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '-' || str[i] == '+')
-			need_cap = 1;
-		else if ('0' <= str[i] && str[i] <= '9')
-			need_cap = 0;
-		if (need_cap)
+		if (ft09_str_is_alpha(str[i]))
 		{
-			if ('a' <= str[i] && str[i] <= 'z')
-			{
-				str[i] += correction;
+			if (need_cap && ft09_str_is_lowercase(str[i]))
+				str[i] += 'A' - 'a';
+			else if (!need_cap && !ft09_str_is_lowercase(str[i]))
+				str[i] -= 'A' - 'a';
+			need_cap = 0;
+		}
+		else if (ft09_str_is_printable(str[i]))
+		{
+			if (ft09_str_is_numeric(str[i]))
 				need_cap = 0;
-			}
+			else
+				need_cap = 1;
 		}
 		i++;
 	}
