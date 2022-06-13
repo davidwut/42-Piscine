eeba3dc688c4b7815719396ca7f3363f431cfffd
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-13 14:23:01 +0200

    prints address and memory TODO read data

--- ex12/ft_print_memory.c
+++ ex12/ft_print_memory.c
@@ -6,94 +6,80 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:06:29 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/12 12:00:53 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/13 14:22:26 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ex12_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	print_hex_formatted(unsigned long hex, int size)
+void	print_addr(unsigned long long addr)
 {
-	char	*translation;
+	char	*base;
 	char	result[16];
 	int i;
 
-	translation = "0123456789abcdef";
+	base = "0123456789abcdef";
 	i = 0;
 	while (i < 16)
 		result[i++] = '0';
 	i = 0;
-	while (hex)
+	while (addr)
 	{
-		result[15 - i] = translation[hex % 16];
-		hex /= 16;
+		result[15 - i] = base[addr % 16];
+		addr /= 16;
 		i++;
 	}
-	i = 16 - size;
-	while (i < 16)
-		ex12_putchar(result[i++]);
-}
-
-void	read_hex_from_mem(void *addr, unsigned int size)
-{
-	int i;
-	unsigned char byte;
-	char *translation;
-
-	translation = "0123456789abcdef";
 	i = 0;
-	while (i < 16 && i < size)
-	{
-		byte = *(((unsigned char *)addr) + i);
-		ex12_putchar(translation[byte / 16]);
-		ex12_putchar(translation[byte % 16]);
-		if((i + 1) % 3)
-			ex12_putchar(' ');
-		i++;
-		size -= 16;
-	}
+	while (i < 16)
+		ft_putchar(result[i++]);
 }
 
-void	read_str_from_mem(void *addr, unsigned int size)
+void	print_mem(unsigned char *addr, unsigned int size)
 {
-	int i;
+	int		i;
+	char	*base;
 
-	while (size)
+	i = 0;
+	base = "0123456789abcdef";
+	while (i < size)
 	{
-		i = 0;
-		while (i < size % 16)
+		if (i % 2 == 0)
+			ft_putchar(' ');
+		if (addr[i] < 16)
 		{
-			ex12_putchar(*(char *)(addr + i));
-			i++;
+			ft_putchar('0');
+			ft_putchar(base[addr[i] % 16]);
 		}
-		size -= 16;
+		else
+		{
+			ft_putchar(base[addr[i] / 16]);
+			ft_putchar(base[addr[i] % 16]);
+		}
+		i++;
 	}
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
 	int i;
-	int lines;
+	unsigned char *c;
 
 	i = 0;
-	lines = 0;
-	while (i < 16 && i < size)
+	c = (unsigned char*) addr;
+	while (i * 16 < size)
 	{
-		print_hex_formatted((unsigned long) (addr + i + 16 * lines), 16);
-		ex12_putchar(':');
-		ex12_putchar(' ');
-		read_hex_from_mem(addr + i + 16 * lines, 16);
-		ex12_putchar(' ');
+		print_addr((unsigned long long)c);
+		ft_putchar(':');
+		print_mem(c, 16);
+		print_data(c, 16);
+		ft_putchar('\n');
+		c += 16;
 		i++;
-		size -= 16;
-		if (i == 16)
-			lines++;
 	}
-
 	return addr;
 }
