efe618e2a7b30c4f45e19689c25a92ef2f192b42
David Wuthrich <dwuthric@c1r16s3.42lausanne.ch>
2022-06-13 16:51:09 +0200

    understood and fixed

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,20 +6,26 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 13:50:54 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/10 14:18:27 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/13 16:50:20 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	res;
 
 	i = 0;
-	while (i < size - 1)
+	res = 0;
+	while (src[res])
+		res++;
+	if (size < 1)
+		return (res);
+	while (i < size - 1 && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[size - 1] = '\0';
-	return (i);
+	dest[i] = '\0';
+	return (res);
 }
