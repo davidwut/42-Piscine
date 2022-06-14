837ff43e5e288db7adcf129ae3cd9b8d77a83030
David Wuthrich <dwuthric@c1r14s2.42lausanne.ch>
2022-06-14 12:49:21 +0200

    added exception

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 19:13:27 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 19:26:29 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/14 12:48:40 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	*ft_range(int min, int max)
 	int	*res;
 	int	i;
 
+	if (min >= max)
+		return (NULL);
 	res = (int *)malloc(sizeof(*res) * (max - min));
 	i = 0;
 	while (i < max - min)
