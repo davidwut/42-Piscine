2e5751156a9d812e054a9d227b0a7a8575a3679b
David Wuthrich <dwuthric@c1r14s2.42lausanne.ch>
2022-06-14 12:34:27 +0200

    solved, ship it

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,34 +6,41 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:02:14 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 18:10:23 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/14 12:31:59 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_swap(char **a, char **b)
-{
-	char	*temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-void	ft_putstr(char *str)
+void	ft_putstr(char *str, char *str2)
 {
 	while (*str)
 	{
 		write(1, str, 1);
 		str++;
 	}
+	while (*str2)
+	{
+		write(1, str2, 1);
+		str2++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	flag;
+	int		i;
+	int		flag;
+	char	*temp;
 
 	flag = 0;
 	while (!flag)
@@ -42,9 +49,11 @@ int	main(int argc, char **argv)
 		flag = 1;
 		while (i < argc - 1)
 		{
-			if (argv[i] > argv[i + 1])
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
 			{
-				ft_swap(&argv[i], &argv[i + 1]);
+				temp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = temp;
 				flag = 0;
 			}
 			i++;
@@ -52,9 +61,6 @@ int	main(int argc, char **argv)
 	}
 	i = 1;
 	while (i < argc)
-	{
-		ft_putstr(argv[i++]);
-		ft_putstr("\n");
-	}
+		ft_putstr(argv[i++], "\n");
 	return (0);
 }
