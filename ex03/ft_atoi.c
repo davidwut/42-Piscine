5d13dc1597f77f1305b2b2018a5170b9ca4db552
David Wuthrich <dwuthric@c1r10s4.42lausanne.ch>
2022-06-15 16:24:45 +0200

    cleaned

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 14:53:34 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/15 13:56:05 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:24:07 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_isspace(char c)
 
 	i = 0;
 	set = " \f\n\r\t\v";
-	while (set[i] != '\0')
+	while (set[i])
 	{
 		if (set[i] == c)
 			return (1);
@@ -32,17 +32,14 @@ int	ft_atoi_helper(char *str, int i, int total, int neg)
 		return (ft_atoi_helper(str, i + 1, total, neg *= -1));
 	if (str[i] == '+' && total == 0)
 		return (ft_atoi_helper(str, i + 1, total, neg));
-	if (!('0' <= str[i] && str[i] <= '9') || str[i] == '\0')
+	if (!('0' <= str[i] && str[i] <= '9') || !str[i])
 		return (total * neg);
 	return (ft_atoi_helper(str, i + 1, total * 10 + str[i] - '0', neg));
 }
 
 int	ft_atoi(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i] && ft_isspace(str[i]))
-		i++;
-	return (ft_atoi_helper(str + i, 0, 0, 1));
+	while (*str && ft_isspace(*str))
+		str++;
+	return (ft_atoi_helper(str, 0, 0, 1));
 }
