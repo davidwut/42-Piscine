c6899d099d87797426e52d5d1404afbaf4d83802
David Wuthrich <dwuthric@c1r10s4.42lausanne.ch>
2022-06-15 13:18:13 +0200

    need norminette, and add -

--- ex04/ft_convert_base.c
+++ ex04/ft_convert_base.c
@@ -6,78 +6,17 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 15:43:01 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/15 13:09:06 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/15 13:17:23 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-int	ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	ft_isspace(char c)
-{
-	int		i;
-	char	*set;
-
-	i = 0;
-	set = " \f\n\r\t\v";
-	while (set[i] != '\0')
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	valid_in_base(char c, char *base_from)
-{
-	int i;
-
-	i = 0;
-	while (base_from[i])
-		if (base_from[i++] == c)
-			return (1);
-	return (0);
-}
-
-int	is_correct_base(char *base)
-{
-	int	i;
-	int	j;
-
-	if (ft_strlen(base) <= 1)
-		return (0);
-	i = 0;
-	while (base[i])
-	{
-		if (base[i] == '+' || base[i] == '-' || ft_isspace(base[i]))
-			return (0);
-		j = i + 1;
-		while (base[j])
-		{
-			if (base[i] == base[j])
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	return (1);
-}
+void	ft_putchar(char c);
+int		ft_strlen(char *str);
+int		ft_isspace(char c);
+int		valid_in_base(char c, char *base_from);
+int		is_correct_base(char *base);
 
 int	adjust_for_neg(char *n, char *base_from)
 {
@@ -128,10 +67,10 @@ int	from_base_to_dec(char *n, char *base)
 
 char	*ft_clean(char *str, char *base_from)
 {
-	char *res;
-	int	i;
-	int	j;
-	int	index;
+	char	*res;
+	int		i;
+	int		j;
+	int		index;
 
 	i = 0;
 	while (ft_isspace(str[i]))
@@ -142,7 +81,7 @@ char	*ft_clean(char *str, char *base_from)
 	while (str[j])
 	{
 		if (!valid_in_base(str[j], base_from))
-			break;
+			break ;
 		j++;
 	}
 	res = (char *)malloc(sizeof(*res) * j - i + 1);
@@ -172,10 +111,6 @@ char	*from_dec_to_base(int n, char *base_to, int neg)
 	}
 	res = (char *)malloc(sizeof(*res) * (i + 1));
 	j = 0;
-	if (neg == -1)
-	{
-		res
-	}
 	while (j < i)
 	{
 		res[j] = temp[100 - i + j];
--- ex04/ft_convert_base2.c
+++ ex04/ft_convert_base2.c
@@ -6,7 +6,74 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 16:12:13 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/14 16:12:18 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/15 13:13:59 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_isspace(char c)
+{
+	int		i;
+	char	*set;
+
+	i = 0;
+	set = " \f\n\r\t\v";
+	while (set[i] != '\0')
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	valid_in_base(char c, char *base_from)
+{
+	int	i;
+
+	i = 0;
+	while (base_from[i])
+		if (base_from[i++] == c)
+			return (1);
+	return (0);
+}
+
+int	is_correct_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) <= 1)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || ft_isspace(base[i]))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
