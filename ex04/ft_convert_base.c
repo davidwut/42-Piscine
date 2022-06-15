52a1f25365d38e2c1ca3384adbfdf2de590bfbb5
David Wuthrich <dwuthric@c1r10s4.42lausanne.ch>
2022-06-15 13:09:41 +0200

    works, problems if mrd is number

--- ex04/ft_convert_base.c
+++ ex04/ft_convert_base.c
@@ -6,11 +6,17 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 15:43:01 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/14 17:37:20 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/15 13:09:06 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 int	ft_strlen(char *str)
 {
@@ -38,6 +44,17 @@ int	ft_isspace(char c)
 	return (0);
 }
 
+int	valid_in_base(char c, char *base_from)
+{
+	int i;
+
+	i = 0;
+	while (base_from[i])
+		if (base_from[i++] == c)
+			return (1);
+	return (0);
+}
+
 int	is_correct_base(char *base)
 {
 	int	i;
@@ -49,7 +66,7 @@ int	is_correct_base(char *base)
 	while (base[i])
 	{
 		if (base[i] == '+' || base[i] == '-' || ft_isspace(base[i]))
-			return (1);
+			return (0);
 		j = i + 1;
 		while (base[j])
 		{
@@ -62,61 +79,123 @@ int	is_correct_base(char *base)
 	return (1);
 }
 
-int	get_neg_sign(char *n, int *i)
+int	adjust_for_neg(char *n, char *base_from)
 {
-	int neg;
+	int	neg;
+	int	i;
 
 	neg = 1;
-	while (n[*i])
-	{
-		if (n[*i] == '-' || n[*i] == '+')
-			if (n[*i] == '-')
-				neg *= -1;
-		*i++;
-	}
-	return neg;
-}
-
-int	from_base_to_dec(char n, char *base)
-{
-	int  i;
-
-	i = 0;
-	while (base[i])
-	{
-		if (n == base[i])
-			return i;
-		i++;
-	}
-	return -1;
-}
-
-char *ft_convert_base(char *n, char *base_from, char *base_to)
-{
-	int i;
-	int j;
-	int neg;
-	char *res;
-	int ret;
-
-	if (!is_correct_base(base_from) || !is_correct_base(base_to))
-		return (NULL);
 	i = 0;
 	while (ft_isspace(n[i]))
 		i++;
-	neg = get_neg_sign(n, &i);
-	res = malloc(sizeof(*res) * ft_strlen(n));
-	if (neg == -1)
-		res[0] = '-';
-	j = neg == -1;
 	while (n[i])
 	{
-		ret = from_base_to_dec(n[i], base_from);
-		if (ret == -1)
-			return (NULL);
-		res[j] = base_to[ret];
-		j++;
+		if (n[i] == '-' || n[i] == '+')
+		{
+			if (n[i] == '-')
+				neg *= -1;
+		}
+		else if (valid_in_base(n[i], base_from))
+			return (neg);
+		else
+			return (0);
 		i++;
 	}
+	return (neg);
+}
+
+int	from_base_to_dec(char *n, char *base)
+{
+	int	i;
+	int	j;
+	int	total;
+
+	i = 0;
+	total = 0;
+	while (n[i])
+	{
+		j = 0;
+		while (base[j])
+		{
+			if (n[i] == base[j])
+				total = total * ft_strlen(base) + j;
+			j++;
+		}
+		i++;
+	}
+	return (total);
+}
+
+char	*ft_clean(char *str, char *base_from)
+{
+	char *res;
+	int	i;
+	int	j;
+	int	index;
+
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+		i++;
+	j = i;
+	while (str[j])
+	{
+		if (!valid_in_base(str[j], base_from))
+			break;
+		j++;
+	}
+	res = (char *)malloc(sizeof(*res) * j - i + 1);
+	index = 0;
+	while (index < j - i)
+	{
+		res[index] = str[i + index];
+		index++;
+	}
+	res[j - i] = '\0';
 	return (res);
+}
+
+char	*from_dec_to_base(int n, char *base_to, int neg)
+{
+	char	temp[100];
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	while (n)
+	{
+		temp[99 - i] = base_to[n % ft_strlen(base_to)];
+		i++;
+		n /= ft_strlen(base_to);
+	}
+	res = (char *)malloc(sizeof(*res) * (i + 1));
+	j = 0;
+	if (neg == -1)
+	{
+		res
+	}
+	while (j < i)
+	{
+		res[j] = temp[100 - i + j];
+		j++;
+	}
+	return (res);
+}
+
+char	*ft_convert_base(char *n, char *base_from, char *base_to)
+{
+	int		ret;
+	char	*cleaned;
+	int		neg;
+
+	if (!is_correct_base(base_from) || !is_correct_base(base_to))
+		return (NULL);
+	neg = adjust_for_neg(n, base_from);
+	if (!neg)
+		return (NULL);
+	cleaned = ft_clean(n, base_from);
+	ret = from_base_to_dec(cleaned, base_from);
+	return (from_dec_to_base(ret, base_to, neg));
 }
