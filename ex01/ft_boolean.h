9e8193d13faf4983f8ee7d337f2e68d079243dff
David Wuthrich <dwuthric@c1r10s4.42lausanne.ch>
2022-06-15 18:06:59 +0200

    understood

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,12 +6,20 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:08:24 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/14 18:09:52 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/15 18:06:35 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
+# include <unistd.h>
+# define EVEN(x) x % 2 == 0
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS TRUE
 
+typedef int	t_bool;
 
 #endif
