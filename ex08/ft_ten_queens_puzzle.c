f01c57bbf058e43dde4e6805476bfc2899443fa9
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-16 17:38:15 +0200

    cleaned

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -6,30 +6,13 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 13:16:45 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/16 10:28:50 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/16 17:37:50 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	i + j right diagonal
 	i - j left diagonal
-	1) Start in the leftmost column
-	2) If all queens are placed
-    	return true
-	3) Try all rows in the current column.
-   		Do following for every tried row.
-    	a) If the queen can be placed safely in this row
-       		then mark this [row, column] as part of the
-       		solution and recursively check if placing
-       		queen here leads to a solution.
-    	b) If placing the queen in [row, column] leads to
-       		a solution then return true.
-    	c) If placing queen doesn't lead to a solution then
-       		unmark this [row, column] (Backtrack) and go to
-       		step (a) to try other rows.
-	4) If all rows have been tried and nothing worked,
-   		return false to trigger backtracking.
-
 */
 
 #include <unistd.h>
