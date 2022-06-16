21f3c98b00890db3c1e41bda4edc904169d28f9a
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-16 11:08:51 +0200

    cleaned, and handles INT_MIN

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,34 +6,20 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 15:42:05 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/15 14:16:28 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/16 11:07:39 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ex04_putchar(char c)
+typedef long long	t_ll;
+
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-int	ex04_ft_isspace(char c)
-{
-	int		i;
-	char	*set;
-
-	i = 0;
-	set = " \f\n\r\t\v";
-	while (set[i] != '\0')
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	ex04_ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int	i;
 
@@ -43,7 +29,7 @@ int	ex04_ft_strlen(char *str)
 	return (i);
 }
 
-int	ex04_is_base_incorrect(char *base)
+int	is_base_incorrect(char *base)
 {
 	int	i;
 	int	j;
@@ -53,7 +39,7 @@ int	ex04_is_base_incorrect(char *base)
 	i = 0;
 	while (base[i])
 	{
-		if (base[i] == '+' || base[i] == '-' || ft_isspace(base[i]))
+		if (base[i] == '+' || base[i] == '-')
 			return (1);
 		j = i + 1;
 		while (base[j])
@@ -67,27 +53,24 @@ int	ex04_is_base_incorrect(char *base)
 	return (0);
 }
 
-void	ft_putnbr_base(int n, char *base)
+void	ft_putnbr_base_helper(t_ll n, char *base)
 {
 	int	baselen;
-	int	i;
 
-	if (ex04_is_base_incorrect(base))
+	if (is_base_incorrect(base))
 		return ;
-	baselen = ex04_ft_strlen(base);
-	i = 0;
-	if (n == -2147483648)
-	{
-		ex04_putchar('-');
-		ex04_putchar('2');
-		n = 147483648;
-	}
+	baselen = ft_strlen(base);
 	if (n < 0)
 	{
-		ex04_putchar('-');
-		n *= -1;
+		ft_putchar('-');
+		n = -n;
 	}
 	if (n > baselen - 1)
-		ft_putnbr_base(n / baselen, base);
-	ex04_putchar(base[(n % baselen)]);
+		ft_putnbr_base_helper(n / baselen, base);
+	ft_putchar(base[(n % baselen)]);
+}
+
+void	ft_putnbr_base(int n, char *base)
+{
+	ft_putnbr_base_helper((t_ll)n, base);
 }
