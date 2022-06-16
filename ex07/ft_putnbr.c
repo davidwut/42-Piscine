200e9dc71b8346b1ce80d4ed795562371a82909a
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-16 14:38:58 +0200

    used my answer from c04

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,38 +6,33 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 16:26:25 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 10:10:02 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/16 14:38:01 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
 	if (nb < 0)
 	{
-		putchar('-');
+		ft_putchar('-');
 		nb *= -1;
 	}
 	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
 	}
-	putchar(nb % 10 + '0');
+	ft_putchar(nb % 10 + '0');
 }
-
-
-#ifdef og
-
-int	main(void)
-{
-	ft_putnbr(-2147483647);
-	return (0);
-}
-
-#endif
