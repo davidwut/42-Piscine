120904aab13486d15e25b2b89285284359eb21a5
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-16 14:12:45 +0200

    it should be fine, not sure why not

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 14:20:50 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 16:41:37 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/16 14:12:18 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,12 @@ void	ft_putstr_non_printable(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (' ' <= str[i] && str[i] <= '~')
-		{
 			ft_putchar(str[i]);
-		}
 		else
-		{
 			ft_puthex(str[i]);
-		}
 		i++;
 	}
 }
