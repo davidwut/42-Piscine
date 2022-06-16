5bc124e4c10ec1e326e50723efa747f049e7640e
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-16 12:50:30 +0200

    corrected and cleaned

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,33 +6,18 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:57:12 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/14 10:06:32 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/16 12:49:08 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft09_str_is_alpha(char c)
-{
-	if (!('a' <= c && c <= 'z'))
-		if (!('A' <= c && c <= 'Z'))
-			return (0);
-	return (1);
-}
-
-int	ft09_str_is_lowercase(char c)
-{
-	if (!('a' <= c && c <= 'z'))
-		return (0);
-	return (1);
-}
-
-int	ft09_str_is_printable(char c)
+int	str_is_printable(char c)
 {
 	if (!(' ' <= c && c <= '~'))
 		return (0);
 	return (1);
 }
 
-int	ft09_str_is_numeric(char c)
+int	str_is_numeric(char c)
 {
 	if (!('0' <= c && c <= '9'))
 		return (0);
@@ -41,28 +26,25 @@ int	ft09_str_is_numeric(char c)
 
 char	*ft_strcapitalize(char *str)
 {
-	int	need_cap;
-	int	i;
+	int		need_cap;
+	int		i;
+	char	c;
 
 	need_cap = 1;
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (ft09_str_is_alpha(str[i]))
-		{
-			if (need_cap && ft09_str_is_lowercase(str[i]))
-				str[i] += 'A' - 'a';
-			else if (!need_cap && !ft09_str_is_lowercase(str[i]))
-				str[i] -= 'A' - 'a';
+		c = str[i];
+		if (need_cap && 'a' <= c && c <= 'z')
+			str[i] -= 32;
+		else if (!need_cap && 'A' <= c && c <= 'Z')
+			str[i] += 32;
+		if (('a' <= c && c <= 'z')
+			|| ('0' <= c && c <= '9')
+			|| ('A' <= c && c <= 'Z'))
 			need_cap = 0;
-		}
-		else if (ft09_str_is_printable(str[i]))
-		{
-			if (ft09_str_is_numeric(str[i]))
-				need_cap = 0;
-			else
-				need_cap = 1;
-		}
+		else
+			need_cap = 1;
 		i++;
 	}
 	return (str);
