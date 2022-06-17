29d817e33c583ab50d6b825b014d39d0cca9de3a
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-17 14:27:44 +0200

    removed semi colon

--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -6,12 +6,12 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 18:08:17 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/15 18:09:28 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/17 14:23:50 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
-# define ABS(x) x < 0 ? -x : x;
+# define ABS(x) x < 0 ? -x : x
 
 #endif
