6ca0f55591af065acc98924fc810226486876ceb
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-17 10:29:05 +0200

    factorial of 0 is 1

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 11:24:36 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 12:10:56 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/17 10:25:33 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@ int	ft_iterative_factorial(int n)
 {
 	int	res;
 
-	if (n <= 0)
+	if (n < 0)
 		return (0);
+	if (n == 0)
+		return (1);
 	res = 1;
 	while (n)
 		res *= (n--);
--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,15 +6,15 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 11:35:01 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 12:11:15 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/17 10:28:41 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 		return (0);
-	if (n == 1)
+	if (n == 0 || n == 1)
 		return (1);
 	return (n * ft_recursive_factorial(n - 1));
 }
