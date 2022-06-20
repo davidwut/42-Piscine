5a796321802ae12cdd00bf481009fdda9e123d11
David Wuthrich <dwuthric@c1r6s4.42lausanne.ch>
2022-06-20 11:47:52 +0200

    corrected bug, and ex05 done

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 14:48:45 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/20 10:22:47 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/20 11:46:47 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strcpy(char *src)
 	unsigned int	i;
 	char			*res;
 
-	res = malloc(sizeof(*res) * ft_strlen(src) + 1);
+	res = malloc(sizeof(*res) * (ft_strlen(src) + 1));
 	if (!res)
 		return (NULL);
 	i = -1;
@@ -43,16 +43,16 @@ t_stock_str	*ft_strs_to_tab(int ac, char **av)
 	t_stock_str	*res;
 	int			i;
 
-	res = malloc(sizeof(*res) * ac + 1);
+	res = malloc(sizeof(*res) * (ac + 1));
 	if (!res)
 		return (NULL);
-	res[ac].size = ac;
+	res[ac].size = 0;
 	res[ac].str = 0;
 	res[ac].copy = 0;
 	i = -1;
 	while (++i < ac)
 	{
-		res[i].size = ac;
+		res[i].size = ft_strlen(av[i]);
 		res[i].str = ft_strcpy(av[i]);
 		res[i].copy = ft_strcpy(av[i]);
 	}
