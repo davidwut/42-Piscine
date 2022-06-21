b2c93278a437f7eff39acdc2e565b8254484b4e3
David Wuthrich <dwuthric@c1r4s4.42lausanne.ch>
2022-06-21 15:34:49 +0200

    maybe fix

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 13:15:48 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/14 15:40:56 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/21 15:34:14 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	ft_strlen(char *str)
 {
 	int	i;
 
-	i = 0;
-	while (str[i])
-		i++;
+	i = -1;
+	while (str[++i])
+		;
 	return (i);
 }
 
@@ -28,45 +28,47 @@ char	*ft_strncat(char *dst, char *src, unsigned int n)
 	unsigned int	dst_size;
 
 	dst_size = 0;
-	i = 0;
-	while (dst[i] != '\0')
-	{
+	i = -1;
+	while (dst[++i] != '\0')
 		dst_size++;
-		i++;
-	}
-	i = 0;
-	while (src[i] != '\0' && i < n)
-	{
+	i = -1;
+	while (src[++i] != '\0' && i < n)
 		dst[dst_size + i] = src[i];
-		i++;
-	}
 	dst[dst_size + i] = '\0';
 	return (dst);
 }
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+char	*init_str(int size)
+{
+	char	*res;
+	int		i;
+
+	res = malloc(sizeof(*res) * size);
+	i = -1;
+	while (++i < size)
+		res[i] = 0;
+	return (res);
+}
+
+char	*ft_strjoin(int size, char **list, char *sep)
 {
 	int		i;
-	char	*res;
 	int		total_size;
+	char	*res;
 
 	if (size <= 0)
-		return ((char *)malloc(0));
-	i = 0;
-	total_size = ft_strlen(sep) * size - 1;
-	while (i < size)
+		return init_str(1);
+	total_size = ft_strlen(sep) * (size - 1);
+	i = -1;
+	while (++i)
+		total_size += ft_strlen(list[i]);
+	res = init_str(total_size + 1);
+	i = -1;
+	while (++i < size)
 	{
-		total_size += ft_strlen(strs[i]);
-		i++;
-	}
-	res = (char *)malloc(sizeof(char) * total_size + 1);
-	i = 0;
-	while (i < size)
-	{
-		ft_strncat(res, strs[i], ft_strlen(strs[i]));
+		res = ft_strncat(res, list[i], ft_strlen(list[i]));
 		if (i != size - 1)
-			ft_strncat(res, sep, ft_strlen(sep));
-		i++;
+			res = ft_strncat(res, sep, ft_strlen(sep));
 	}
 	return (res);
 }
