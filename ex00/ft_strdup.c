2137139b0af480ed1e4c2985b22b620c903e72fb
David Wuthrich <dwuthric@c1r4s4.42lausanne.ch>
2022-06-21 13:14:48 +0200

    potentially fixed KO

--- ex00/ft_strdup.c
+++ ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:56:19 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/13 19:10:30 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/21 13:12:18 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,10 @@ char	*ft_strdup(char *src)
 	len = 0;
 	while (src[len])
 		len++;
-	res = (char *)malloc(sizeof(*res) * len);
-	len = 0;
-	while (src[len])
-	{
+	res = (char *)malloc(sizeof(*res) * (len + 1));
+	len = -1;
+	while (src[++len])
 		res[len] = src[len];
-		len++;
-	}
+	res[len] = 0;
 	return (res);
 }
