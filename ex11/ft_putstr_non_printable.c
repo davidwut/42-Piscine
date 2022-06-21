f775b004d1d00df19d7f138518fa35696a4b43a4
David Wuthrich <dwuthric@c1r4s4.42lausanne.ch>
2022-06-21 10:30:43 +0200

    fixed char overflow

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 14:20:50 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/16 14:12:18 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/21 10:17:47 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_puthex(char c)
+void	ft_puthex(int c, int pass)
 {
 	char	*hex;
 
 	hex = "0123456789abcdef";
-	ft_putchar('\\');
-	ft_putchar(hex[c / 16]);
+	if (c < 0)
+		c += 256;
+	if (!pass)
+		ft_putchar('\\');
+	if (c > 15)
+		ft_puthex(c / 16, 1);
+	if (!pass && c < 16)
+		ft_putchar('0');
 	ft_putchar(hex[c % 16]);
 }
 
@@ -37,7 +43,7 @@ void	ft_putstr_non_printable(char *str)
 		if (' ' <= str[i] && str[i] <= '~')
 			ft_putchar(str[i]);
 		else
-			ft_puthex(str[i]);
+			ft_puthex(str[i], 0);
 		i++;
 	}
 }
