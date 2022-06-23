5a6049846a2332179a279f00af484de6c20c82f5
David Wuthrich <dwuthric@c1r6s1.42lausanne.ch>
2022-06-23 17:11:57 +0200

    previously was sorted only in ascending order, now its any sorted

--- ex04/ft_is_sort.c
+++ ex04/ft_is_sort.c
@@ -6,17 +6,25 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 14:50:23 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/22 14:54:52 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/23 17:07:57 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_sort(int *arr, int length, int (*f)(int, int))
 {
 	int	i;
+	int	increasing;
+	int	decreasing;
 
+	increasing = 0;
+	decreasing = 0;
 	i = -1;
 	while (++i < length - 1)
-		if ((*f)(arr[i], arr[i + 1]) > 0)
-			return (0);
-	return (1);
+	{
+		if ((*f)(arr[i], arr[i + 1]) >= 0)
+			decreasing++;
+		if ((*f)(arr[i], arr[i + 1]) <= 0)
+			increasing++;
+	}
+	return (increasing == i || decreasing == i);
 }
