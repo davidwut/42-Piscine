19d7bdc86baa314b346a1afc1b3157bee3981760
David Wuthrich <dwuthric@c1r6s1.42lausanne.ch>
2022-06-24 13:39:20 +0200

    im a goddamn genius jkjk

---
+++ ex07/ft_btree.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 18:07:05 by dwuthric          #+#    #+#             */
+/*   Updated: 2022/06/23 18:35:04 by dwuthric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+# include <stdlib.h>
+
+typedef struct s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}	t_btree;
+t_btree	*btree_create_node(void *item);
+#endif
