d3b008533e087b42475955680a5a22d19d4c0eec
David Wuthrich <dwuthric@c2r4s3.42lausanne.ch>
2022-06-25 13:08:09 +0200

    updated

--- ex00/includes/ft.h
+++ ex00/includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 11:23:31 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/06/25 12:45:30 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/06/25 13:07:46 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,5 @@ int 	ft_atoi(char *a);
 void	ft_putstr(char *str);
 t_list	*ft_create_elem(unsigned int value, char *text);
 void	ft_sorted_list_insert(t_list **begin, unsigned int value, char *text, int (*cmp)());
+int	cmp_map(t_map *a, t_map *b);
 #endif
